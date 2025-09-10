0e529cc3d25fa75ce1ad248e23aa9694cdd6fccc
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2025-09-10 13:54:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.sidebar.subheader("Upload Student Data")
 uploaded_file = st.sidebar.file_uploader("Upload CSV or Excel file", type=["csv", "xlsx"])
 
 # Required columns for validation
-REQUIRED_COLUMNS = ["Roll Number", "LeetCode URL", "HackerRank URL"]
+REQUIRED_COLUMNS = ["Roll Number", "CGPA", "Backlogs","LeetCode URL", "HackerRank URL"]
 
 @st.cache_data
 def load_student_data(file):
@@ -167,4 +167,5 @@ elif choice == "Bulk Data Download":
             file_name="Enriched_Student_Data.xlsx",
             mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
         )
+
 
